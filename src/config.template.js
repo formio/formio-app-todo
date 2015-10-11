688d8b8f17ba3ce58829183206f666e27fa98e78
Travis Tidwell <codertravist@gmail.com>
2015-10-11 11:24:02 -0500

    Update config.template.js

--- src/config.template.js
+++ src/config.template.js
@@ -1,4 +1,9 @@
 angular.module('formioAppTodo').constant('AppConfig', {
+{% if domain %}
+  appUrl: '{{ domain }}',
+  apiUrl: '{{ domain }}'
+{% else %}
   appUrl: '{{ protocol }}://{{ path }}.{{ host }}',
   apiUrl: '{{ protocol }}://api.{{ host }}'
+{% endif %}
 });
