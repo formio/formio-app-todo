1ade00812deb36d5a575fcff4d2dab7b19760c97
Travis Tidwell <travis@form.io>
2016-01-31 20:18:40 -0600

    Fixing the config file.

--- src/config.js
+++ src/config.js
@@ -1,19 +1,21 @@
-var APP_URL = 'https://fxmumuwjcdfwmoj.form.io';
+var APP_URL = 'https://yourapp.form.io';
 var API_URL = 'https://api.form.io';
 
 // Parse query string
 var query = {};
-location.search.substr(1).split("&").forEach(function(item) {
+location.search.replace(/^[^a-z]+/, '').split("&").forEach(function(item) {
     query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
 });
 
+var appUrl = query.appUrl || APP_URL;
+var apiUrl = query.apiUrl || API_URL;
 angular.module('formioAppTodo').constant('AppConfig', {
-  appUrl: query.appUrl || APP_URL,
-  apiUrl: query.apiUrl || API_URL,
+  appUrl: appUrl,
+  apiUrl: apiUrl,
   forms: {
-    userForm: APP_URL + '/user',
-    userLoginForm: APP_URL + '/user/login',
-    userRegisterForm: APP_URL + '/user/register',
-    todoForm: APP_URL + '/todo'
+    userForm: appUrl + '/user',
+    userLoginForm: appUrl + '/user/login',
+    userRegisterForm: appUrl + '/user/register',
+    todoForm: appUrl + '/todo'
   },
 });
